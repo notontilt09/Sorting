77b57d1c09e57f551d730fbb3e3f66977a483feb
Daniel Weinman <namniewd@gmail.com>
2019-04-28 11:25:42 -0400

    iterative solution for bubble sort, prob could be optimized

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,15 +18,22 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    swaps = 0
-    for i in range(0, len(arr) - 1):
-        if arr[i] > arr[i+1]:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            swaps += 1
-    if swaps > 0:
-        bubble_sort(arr)
-    return arr
+    #### RECURSIVE
+    # swaps = 0
+    # for i in range(0, len(arr) - 1):
+    #     if arr[i] > arr[i+1]:
+    #         arr[i], arr[i+1] = arr[i+1], arr[i]
+    #         swaps += 1
+    # if swaps > 0:
+    #     bubble_sort(arr)
+    # return arr
 
+    ## ITERATIVE SOLUTION
+    for i in range(0, len(arr)):
+      for j in range(0, len(arr)-1):
+          if arr[j] > arr[j+1]:
+              arr[j+1], arr[j] = arr[j], arr[j+1]
+    return arr
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
