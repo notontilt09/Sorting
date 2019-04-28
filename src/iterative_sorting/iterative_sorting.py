ed3c6b06380eda6a3fbf95d830f0d721c0baf461
Daniel Weinman <namniewd@gmail.com>
2019-04-28 11:06:47 -0400

    wrote bubble sort, but recursively, refactor for iterative

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,7 +18,13 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    swaps = 0
+    for i in range(0, len(arr) - 1):
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            swaps += 1
+    if swaps > 0:
+        bubble_sort(arr)
     return arr
 
 
